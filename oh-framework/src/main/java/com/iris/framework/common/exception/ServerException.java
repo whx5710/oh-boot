f006917dc50774b6ea6c51521276235b7ffbed8c
王小费 <whx5710@qq.com>
2023-12-17 21:50:40 +0800

    异常处理完善，增加堆栈信息打印

--- oh-framework/src/main/java/com/iris/framework/common/exception/ServerException.java
+++ oh-framework/src/main/java/com/iris/framework/common/exception/ServerException.java
@@ -18,19 +18,32 @@ public class ServerException extends RuntimeException {
 
     private final Logger log = LoggerFactory.getLogger(ServerException.class);
 
+    /**
+     * 错误代码
+     */
     private int code;
+    /**
+     * 提示消息
+     */
     private String msg;
+
+    /**
+     * 堆栈信息
+     * 一般不返回到前端，供后台查看错误原因
+     */
+    private String stackInfo;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (!(o instanceof ServerException that)) return false;
-        return getCode() == that.getCode() && Objects.equals(getMsg(), that.getMsg());
+        if (o == null || getClass() != o.getClass()) return false;
+        ServerException that = (ServerException) o;
+        return code == that.code && Objects.equals(msg, that.msg) && Objects.equals(stackInfo, that.stackInfo);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCode(), getMsg());
+        return Objects.hash(code, msg, stackInfo);
     }
 
     public int getCode() {
@@ -49,17 +62,44 @@ public class ServerException extends RuntimeException {
         this.msg = msg;
     }
 
+    public String getStackInfo() {
+        return stackInfo;
+    }
+
+    public void setStackInfo(String stackInfo) {
+        this.stackInfo = stackInfo;
+    }
+
+    /**
+     *
+     * @param msg 错误信息提示
+     */
     public ServerException(String msg) {
         super(msg);
         this.code = ErrorCode.INTERNAL_SERVER_ERROR.getCode();
         this.msg = msg;
+        this.stackInfo = msg;
         log.error("【{}】{}", code, msg);
+    }
+
+    /**
+     *
+     * @param msg 提示给前端信息
+     * @param stackInfo 堆栈信息
+     */
+    public ServerException(String msg, String stackInfo) {
+        super(msg);
+        this.code = ErrorCode.INTERNAL_SERVER_ERROR.getCode();
+        this.msg = msg;
+        this.stackInfo = stackInfo;
+        log.error("【{}】{} 异常提示:{}", code, msg, stackInfo);
     }
 
     public ServerException(ErrorCode errorCode) {
         super(errorCode.getMsg());
         this.code = errorCode.getCode();
         this.msg = errorCode.getMsg();
+        this.stackInfo = errorCode.getMsg();
         log.error("【{}】{}", this.code, this.msg);
     }
 
@@ -67,6 +107,7 @@ public class ServerException extends RuntimeException {
         super(msg, e);
         this.code = ErrorCode.INTERNAL_SERVER_ERROR.getCode();
         this.msg = msg;
+        this.stackInfo = msg;
         log.error("【{}】{}", this.code, this.msg);
     }
 
