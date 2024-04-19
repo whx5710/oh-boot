91d37d1ccd9c75ebec060988b89a2e6f17e44a65
王小费 <591797912@qq.com>
2024-04-19 16:16:33 +0800

    鉴权认证结构调整

--- oh-module/oh-module-system/src/main/java/com/iris/system/security/config/SecurityConfig.java
+++ oh-module/oh-module-system/src/main/java/com/iris/system/security/config/SecurityConfig.java
@@ -1,29 +1,18 @@
-package com.iris.framework.security.config;
+package com.iris.system.security.config;
 
-import com.iris.framework.security.exception.SecurityAuthenticationEntryPoint;
 import com.iris.framework.security.mobile.MobileAuthenticationProvider;
 import com.iris.framework.security.mobile.MobileUserDetailsService;
 import com.iris.framework.security.mobile.MobileVerifyCodeService;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -35,22 +24,17 @@ import java.util.List;
  *
  */
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
-    private final OncePerRequestFilter authenticationTokenFilter;
-    private final PermitResource permitResource;
     private final UserDetailsService userDetailsService;
     private final MobileUserDetailsService mobileUserDetailsService;
     private final MobileVerifyCodeService mobileVerifyCodeService;
     private final PasswordEncoder passwordEncoder;
     private final ApplicationEventPublisher applicationEventPublisher;
 
-    public SecurityConfig(OncePerRequestFilter authenticationTokenFilter, PermitResource permitResource, UserDetailsService userDetailsService,
+    public SecurityConfig(UserDetailsService userDetailsService,
                           MobileUserDetailsService mobileUserDetailsService, MobileVerifyCodeService mobileVerifyCodeService,
                           PasswordEncoder passwordEncoder, ApplicationEventPublisher applicationEventPublisher) {
-        this.authenticationTokenFilter = authenticationTokenFilter;
-        this.permitResource = permitResource;
+
         this.userDetailsService = userDetailsService;
         this.mobileUserDetailsService = mobileUserDetailsService;
         this.mobileVerifyCodeService = mobileVerifyCodeService;
@@ -81,26 +65,5 @@ public class SecurityConfig {
         ProviderManager providerManager = new ProviderManager(providerList);
         providerManager.setAuthenticationEventPublisher(new DefaultAuthenticationEventPublisher(applicationEventPublisher));
         return providerManager;
-    }
-
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // 忽略授权的地址列表
-        List<String> permitList = permitResource.getPermitList();
-        String[] permits = permitList.toArray(new String[0]);
-        // 相关配置请查看官方例子
-        // https://docs.spring.io/spring-security/site/docs/current/api/org/springframework/security/config/annotation/web/builders/HttpSecurity.html
-        http.addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
-                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(permits).permitAll()
-                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .exceptionHandling((authorizeHttpRequests) -> authorizeHttpRequests.authenticationEntryPoint(new SecurityAuthenticationEntryPoint()))
-                .headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .csrf(AbstractHttpConfigurer::disable)
-        ;
-        return http.build();
     }
 }
