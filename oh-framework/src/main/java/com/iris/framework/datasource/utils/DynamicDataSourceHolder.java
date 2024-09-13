9206e9e3447335325a8e4c0a937daed10a0010ba
王小费 <whx5710@qq.com>
2024-09-13 22:29:49 +0800

    优化线程切换

--- oh-framework/src/main/java/com/iris/framework/datasource/utils/DynamicDataSourceHolder.java
+++ oh-framework/src/main/java/com/iris/framework/datasource/utils/DynamicDataSourceHolder.java
@@ -1,9 +1,5 @@
 package com.iris.framework.datasource.utils;
 
-import com.iris.framework.common.constant.Constant;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * 数据源切换处理
  * DynamicDataSourceHolder类主要是设置当前线程的数据源名称
@@ -11,7 +7,6 @@ import org.slf4j.LoggerFactory;
  * @author 王小费 whx5710@qq.com
  */
 public class DynamicDataSourceHolder {
-    private final static Logger log = LoggerFactory.getLogger(DynamicDataSourceHolder.class);
 
     /**
      * 保存动态数据源名称
@@ -21,25 +16,18 @@ public class DynamicDataSourceHolder {
      * 设置/切换数据源，决定当前线程使用哪个数据源
      */
     public static void setDynamicDataSourceKey(String key){
-        // log.info("数据源切换为：{}",key);
         DYNAMIC_DATASOURCE_KEY.set(key);
     }
     /**
      * 获取动态数据源名称，默认使用mater数据源
      */
     public static String getDynamicDataSourceKey(){
-        String key = DYNAMIC_DATASOURCE_KEY.get();
-        if(key == null){
-            key = Constant.MASTER_DB;
-            log.warn("[线程]未获取到数据源，切换到[{}]", key);
-        }
-        return key;
+        return DYNAMIC_DATASOURCE_KEY.get();
     }
     /**
      * 移除当前数据源
      */
     public static void removeDynamicDataSourceKey(){
-        // log.info("移除数据源：{}",DYNAMIC_DATASOURCE_KEY.get());
         DYNAMIC_DATASOURCE_KEY.remove();
     }
 }
