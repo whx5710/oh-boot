b04dde6c0369697675992eec061bf56da0f8fbac
王小费 <whx5710@qq.com>
2025-03-01 23:26:16 +0800

    反射工具类增加方法

--- oh-core/src/main/java/com/finn/core/utils/ReflectUtil.java
+++ oh-core/src/main/java/com/finn/core/utils/ReflectUtil.java
@@ -3,6 +3,9 @@ package com.finn.core.utils;
 import org.springframework.util.ReflectionUtils;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * 反射工具类
@@ -20,7 +23,7 @@ public class ReflectUtil {
      * @throws NoSuchFieldException 异常
      */
     public static Object getValue(Object object, String fieldName) throws NoSuchFieldException {
-        Field field = object.getClass().getDeclaredField(fieldName);
+        Field field = getField(object, fieldName);
         //如果有，设置Accessible为true,启用安全检查（为true时可以使用反射访问私有变量，否则不能访问私有变量）
         field.setAccessible(true);
         return ReflectionUtils.getField(field, object);
@@ -34,8 +37,61 @@ public class ReflectUtil {
      * @throws NoSuchFieldException 异常
      */
     public static void setValue(Object object,String fieldName, Object value) throws NoSuchFieldException {
-        Field field = object.getClass().getDeclaredField(fieldName);
-        field.setAccessible(true);
-        ReflectionUtils.setField(field, object, value);
+        setValue(object, fieldName, value, true);
     }
+
+    /**
+     * 设置属性值，无对应属性需捕获异常
+     * @param object 对象
+     * @param fieldName 属性名
+     * @param value 值
+     * @param cover 是否强制覆盖原来的值，false时，只覆盖为null的属性值
+     * @throws NoSuchFieldException 异常
+     */
+    public static void setValue(Object object,String fieldName, Object value, Boolean cover) throws NoSuchFieldException {
+        Field field = getField(object, fieldName);
+        //如果有，设置Accessible为true,启用安全检查（为true时可以使用反射访问私有变量，否则不能访问私有变量）
+        field.setAccessible(true);
+        if(cover || ReflectionUtils.getField(field, object) == null){
+            ReflectionUtils.setField(field, object, value);
+        }
+    }
+
+    /**
+     * 获取 Field
+     * @param object 对象
+     * @param fieldName 属性名
+     * @return Field
+     * @throws NoSuchFieldException 异常
+     */
+    public static Field getField(Object object, String fieldName) throws NoSuchFieldException {
+        Class<?> clazz = object.getClass();
+        List<Field> list = getFields(clazz);
+        if(!list.isEmpty()){
+            for(Field field: list){
+                if(field.getName().equals(fieldName)){
+                    return field;
+                }
+            }
+        }else{
+            throw new NoSuchFieldException();
+        }
+        return object.getClass().getDeclaredField(fieldName);
+    }
+
+    /**
+     * 获取所有 Field
+     * @param clazz 类
+     * @return list
+     */
+    public static List<Field> getFields(Class<?> clazz){
+        List<Field> fields = new ArrayList<>();
+        while (clazz != null){
+            fields.addAll(new ArrayList<>(Arrays.asList(clazz.getDeclaredFields())));
+            // 父级类中的属性
+            clazz = clazz.getSuperclass();
+        }
+        return fields;
+    }
+
 }
