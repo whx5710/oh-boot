7f11421dd46710116ddfcdf2d36490918f675a6c
王小费 <591797912@qq.com>
2025-03-12 18:09:24 +0800

    sql参数拼接

---
+++ oh-core/src/main/java/com/finn/core/entity/Parameter.java
@@ -0,0 +1,60 @@
+package com.finn.core.entity;
+
+public class Parameter extends SuperEntity{
+
+    /**
+     * 列名 user_name
+     */
+    String colName;
+    /**
+     * 字段 userName
+     */
+    String field;
+    // 值
+    Object value;
+    // 表达式
+    String expression = "eq";
+
+    public Parameter(){
+
+    }
+
+    public Parameter(String field, String expression, Object value, String colName){
+        this.field = field;
+        this.expression = expression;
+        this.value = value;
+        this.colName = colName;
+    }
+
+    public String getColName() {
+        return colName;
+    }
+
+    public void setColName(String colName) {
+        this.colName = colName;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    public String getExpression() {
+        return expression;
+    }
+
+    public void setExpression(String expression) {
+        this.expression = expression;
+    }
+}
---
+++ oh-framework/src/main/java/com/finn/framework/utils/ParameterBuilder.java
@@ -0,0 +1,103 @@
+package com.finn.framework.utils;
+
+import cn.hutool.core.lang.func.Func1;
+import cn.hutool.core.lang.func.LambdaUtil;
+import com.finn.core.entity.Parameter;
+import com.finn.core.exception.ServerException;
+import com.finn.core.utils.ReflectUtil;
+import com.finn.framework.datasource.annotations.TableField;
+import com.finn.framework.query.Query;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParameterBuilder<T> extends Query {
+
+    public static final String EQ = "eq"; // 等于
+    public static final String NE = "ne"; // 不等于
+    public static final String LIKE = "like"; // 模糊查询
+    public static final String IN = "in"; // 模糊查询
+
+    List<Parameter> parameters = new ArrayList();
+
+    Class<T> clazz;
+
+    public Class<T> getClazz() {
+        return clazz;
+    }
+
+    public void setClazz(Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    public static <T> ParameterBuilder<T> of(Class<T> clazz) {
+        ParameterBuilder parameterBuilder = new ParameterBuilder();
+        parameterBuilder.setClazz(clazz);
+        return parameterBuilder;
+    }
+
+    /**
+     * 等于
+     * @param function
+     * @param value
+     * @return
+     */
+    public ParameterBuilder<T> eq(Func1<T, ?> function, Object value) {
+        String fieldName = LambdaUtil.getMethodName(function);
+        this.parameters.add(new Parameter(fieldName, EQ, value, getColName(fieldName)));
+        return this;
+    }
+
+    /**
+     * 不等于
+     * @param function
+     * @param value
+     * @return
+     */
+    public ParameterBuilder<T> ne(Func1<T, ?> function, Object value) {
+        String fieldName = LambdaUtil.getMethodName(function);
+        this.parameters.add(new Parameter(fieldName, NE, value, getColName(fieldName)));
+        return this;
+    }
+
+    /**
+     * 模糊查询
+     * @param function
+     * @param value
+     * @return
+     */
+    public ParameterBuilder<T> like(Func1<T, ?> function, Object value) {
+        String fieldName = LambdaUtil.getMethodName(function);
+        this.parameters.add(new Parameter(fieldName, LIKE, value, getColName(fieldName)));
+        return this;
+    }
+
+    public List<Parameter> list() {
+        return this.parameters;
+    }
+
+    /**
+     * 获取列名
+     * @param fieldName 属性名
+     * @return 列名
+     */
+    private String getColName(String fieldName) {
+        Field field = null;
+        try {
+            field = ReflectUtil.getFieldByClass(clazz, fieldName);
+        } catch (NoSuchFieldException e) {
+            throw new ServerException("【" + fieldName + "】字段未找到，请检查");
+        }
+        if(field.isAnnotationPresent(TableField.class)){ // 判断是否有该注解
+            TableField annotation = field.getAnnotation(TableField.class);
+            if(annotation.exists()){
+                return annotation.value();
+            }else{
+                throw new ServerException("【" + fieldName + "】字段不存在，请检查");
+            }
+        }else{
+            return fieldName;
+        }
+    }
+}
