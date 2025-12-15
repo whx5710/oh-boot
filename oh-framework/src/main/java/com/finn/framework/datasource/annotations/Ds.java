dbfe4b184eac5d01d2bcf445b3f81bf3dddaf0a7
王小费 <591797912@qq.com>
2025-12-15 16:19:31 +0800

    完善注释内容

--- oh-framework/src/main/java/com/finn/framework/datasource/annotations/Ds.java
+++ oh-framework/src/main/java/com/finn/framework/datasource/annotations/Ds.java
@@ -4,7 +4,8 @@ import java.lang.annotation.*;
 
 /**
  * 切换数据源注解<br/>
- * 建议注解到service层，注解到mapper层时，需注意有没有继承BaseMapper，父类方法不能切换
+ * 建议注解到service层，注解到mapper层时，需注意有没有继承BaseMapper，父类方法不能切换<br/>
+ * 注意：如果在Ds注解的类或方法中，调用其他有Ds注解的方法，数据源切换会失效
  * @author 王小费 whx5710@qq.com
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
