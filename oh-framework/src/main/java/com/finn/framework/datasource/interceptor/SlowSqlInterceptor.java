43bd36bca1b185fddaafc776dca0b33554256fc2
王小费 <153594370+whx5710@users.noreply.github.com>
2026-03-14 18:41:04 +0800

    慢sql拦截

---
+++ oh-framework/src/main/java/com/finn/framework/datasource/interceptor/SlowSqlInterceptor.java
@@ -0,0 +1,64 @@
+package com.finn.framework.datasource.interceptor;
+
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.plugin.*;
+import org.apache.ibatis.session.ResultHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.sql.Statement;
+
+@Intercepts({
+        // 拦截查询方法
+        @Signature(type = StatementHandler.class,method = "query",args = {Statement.class, ResultHandler.class}),
+        // 拦截更新方法（insert/update/delete）
+        @Signature(type = StatementHandler.class,method = "update", args = {Statement.class})
+})
+@Component
+public class SlowSqlInterceptor implements Interceptor {
+
+    private final Logger log = LoggerFactory.getLogger(SlowSqlInterceptor.class);
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        // 1. 记录开始时间
+        long startTime = System.currentTimeMillis();
+
+        try {
+            // 2. 执行原方法（继续SQL执行流程）
+            return invocation.proceed();
+        } finally {
+            // 3. 计算执行耗时（无论成功失败都记录）
+            long costTime = System.currentTimeMillis() - startTime;
+
+            // 5. 判断是否慢查询
+            // 慢查询阈值（毫秒），可通过配置文件注入
+            long slowThreshold = 500;
+            if (costTime > slowThreshold) {
+                // 4. 获取SQL语句和参数
+                StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+                String sql = statementHandler.getBoundSql().getSql();  // 获取SQL语句（带?占位符）
+                Object parameterObject = statementHandler.getBoundSql().getParameterObject();  // 获取参数
+                log.warn("[慢查询警告] 执行时间: {}ms, SQL: {}, 参数: {}",
+                        costTime, sql, parameterObject);
+            }else{
+                log.debug("[SQL监控] 执行时间: {}ms", costTime);
+            }
+        }
+    }
+
+    @Override
+    public Object plugin(Object target) {
+        return Interceptor.super.plugin(target);
+    }
+
+//    @Override
+//    public void setProperties(Properties properties) {
+//        // 从配置文件读取阈值（如application.yml中配置）
+//        String threshold = properties.getProperty("slowThreshold");
+//        if (threshold != null) {
+//            slowThreshold = Long.parseLong(threshold);
+//        }
+//    }
+}
