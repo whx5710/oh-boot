0985c6875771e852f43872ea9bfb0c62fdf684fa
Finn <whx5710@qq.com>
2026-06-28 11:06:09 +0800

    更新表数据-删除临时文件

--- oh-system/src/main/java/com/finn/system/service/impl/AttachmentServiceImpl.java
+++ oh-system/src/main/java/com/finn/system/service/impl/AttachmentServiceImpl.java
@@ -2,6 +2,8 @@ package com.finn.system.service.impl;
 
 import com.finn.framework.cache.RedisCache;
 import com.finn.framework.cache.RedisKeys;
+import com.finn.framework.datasource.wrapper.QueryWrapper;
+import com.finn.framework.datasource.wrapper.UpdateWrapper;
 import com.finn.framework.entity.HashDto;
 import com.finn.framework.entity.PageResult;
 import com.finn.framework.utils.AssertUtils;
@@ -18,10 +20,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import static com.finn.framework.common.constant.Constant.ASC;
 
 /**
  * 附件管理
@@ -101,6 +106,44 @@ public class AttachmentServiceImpl implements AttachmentService {
                 }
             } catch (Exception e) {
                 log.error("保存文件异常：{}", ExceptionUtils.getExceptionMessage(e));
+            }
+
+            // 缓存临时文件队列，在文件服务中读取，删除临时文件
+            if(redisCache.getListSize(RedisKeys.getTmpFileCacheKey()) == 0){
+                // 查询48小时前的临时文件
+                LocalDateTime time = LocalDateTime.now();
+                time = time.minusHours(48);
+                QueryWrapper<AttachmentEntity> queryWrapper = new QueryWrapper<>();
+                queryWrapper.eq(AttachmentEntity::getDbStatus, 1).eq(AttachmentEntity::getTmpFlag, 1)
+                        .le(AttachmentEntity::getCreateTime, time).orderBy(AttachmentEntity::getCreateTime, ASC)
+                        .page(1, 10);
+                List<AttachmentEntity> list = attachmentMapper.listByWrapper(queryWrapper);
+                if(list != null && !list.isEmpty()){
+                    for (AttachmentEntity item: list){
+                        HashDto hashDto = new HashDto();
+                        hashDto.put("id", item.getId());
+                        hashDto.put("url", item.getUrl());
+                        redisCache.leftPush(RedisKeys.getTmpFileCacheKey(), hashDto);
+                    }
+                }
+            }
+
+            // 更新已删除的文件状态(文件服务已将文件删除)
+            if(redisCache.getListSize(RedisKeys.getTmpFileDelKey()) > 0){
+                List<Long> ids = new ArrayList<>();
+                for(int i = 0; i < 10; i++){
+                    Object object = redisCache.rightPop(RedisKeys.getTmpFileDelKey());
+                    if(object == null){
+                        break;
+                    }
+                    HashDto hashDto = (HashDto) object;
+                    ids.add(hashDto.getLong("id"));
+                }
+                if(!ids.isEmpty()){
+                    attachmentMapper.updateByWrapper(UpdateWrapper.of(AttachmentEntity.class)
+                            .set(AttachmentEntity::getDbStatus, 0).set(AttachmentEntity::getUpdateTime, LocalDateTime.now())
+                            .eq(AttachmentEntity::getDbStatus, 1).in(AttachmentEntity::getId, ids));
+                }
             }
         }, 10, 150, TimeUnit.SECONDS);
     }
