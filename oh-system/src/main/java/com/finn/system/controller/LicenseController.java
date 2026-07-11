26997e76b8a578c3f49cd210b7765af748a786bf
Finn <whx5710@qq.com>
2026-07-11 17:50:28 +0800

    增加license证书管理

---
+++ oh-system/src/main/java/com/finn/system/controller/LicenseController.java
@@ -0,0 +1,43 @@
+package com.finn.system.controller;
+
+import com.finn.framework.entity.Result;
+import com.finn.framework.license.LicenseManager;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/license")
+public class LicenseController {
+
+    private final LicenseManager licenseManager;
+
+    public LicenseController(LicenseManager licenseManager) {
+        this.licenseManager = licenseManager;
+    }
+
+    /**
+     * 获取当前服务器机器码，提供给授权方
+     */
+    @GetMapping("/machine-code")
+    public Result<String> getMachineCode() {
+        return Result.ok(licenseManager.getMachineCode());
+    }
+
+    /**
+     * 上传 License 文件，安装后实时生效
+     */
+    @PostMapping("/install")
+    public Result<Void> install(@RequestBody String licenseJson) {
+        licenseManager.install(licenseJson);
+        return Result.ok();
+    }
+
+    /**
+     * 查看 License 状态
+     */
+    @GetMapping("/info")
+    public Result<Map<String, Object>> info() {
+        return Result.ok(licenseManager.getLicenseInfo());
+    }
+}
