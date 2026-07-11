ecfffb9379d984cc7cc425efc5a20d61a9cda4fa
Finn <whx5710@qq.com>
2026-07-12 00:35:15 +0800

    优化证书安装

--- oh-system/src/main/java/com/finn/system/controller/LicenseController.java
+++ oh-system/src/main/java/com/finn/system/controller/LicenseController.java
@@ -1,7 +1,10 @@
 package com.finn.system.controller;
 
+import com.finn.framework.aop.annotations.Idempotent;
 import com.finn.framework.entity.Result;
+import com.finn.framework.license.License;
 import com.finn.framework.license.LicenseManager;
+import com.finn.framework.utils.JsonUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -27,9 +30,10 @@ public class LicenseController {
     /**
      * 上传 License 文件，安装后实时生效
      */
+    @Idempotent(keyPrefix = "license", message = "请求过于频繁，请稍后再试", timeout = 10, limit = true)
     @PostMapping("/install")
-    public Result<String> install(@RequestBody String licenseJson) {
-        licenseManager.install(licenseJson);
+    public Result<String> install(@RequestBody License license) {
+        licenseManager.install(JsonUtils.toJsonString(license));
         return Result.ok("安装成功");
     }
 
