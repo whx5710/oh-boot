e9d7005b81ec2db4edf178ad5f2954046431de8a
Finn <whx5710@qq.com>
2026-08-23 00:20:49 +0800

    精简数据

--- oh-module/oh-urban/src/main/java/com/finn/urban/vo/PointVO.java
+++ oh-module/oh-urban/src/main/java/com/finn/urban/vo/PointVO.java
@@ -1,5 +1,7 @@
 package com.finn.urban.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class PointVO {
@@ -7,11 +9,13 @@ public class PointVO {
     /**
      * 经度，范围为 -180~180，负数表示西经，使用 gcj02 国测局坐标系
      */
+    @JsonProperty("lng")
     private BigDecimal longitude;
 
     /**
      * 纬度，范围为 -90~90，负数表示南纬，使用 gcj02 国测局坐标系
      */
+    @JsonProperty("lat")
     private BigDecimal latitude;
 
     public PointVO(BigDecimal longitude, BigDecimal latitude){
